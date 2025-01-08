abd924cd8c7476258b0ae88f9a8b17d54bb1c125
MiracleWorker <entsav.go@yahoo.com>
2025-01-08 07:47:52 -0800

    API created

--- src/formfiller/main.py
+++ src/formfiller/main.py
@@ -1,16 +1,104 @@
 #!/usr/bin/env python
 import sys
-from formfiller.crew import Formfiller
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task, before_kickoff, after_kickoff
+from PyPDFForm import PdfWrapper
 import json
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Dict, Any
+from typing import List, Union, Dict, Any
 
+
+
+class Pydantic(BaseModel):
+   field_key : str
+   answer : Union[str, int, bool]
+
+class outputPydantic(BaseModel):
+	response: List[Pydantic]
+@CrewBase
+class Formfiller():
+	"""Formfiller crew"""
+
+	agents_config = 'config/agents.yaml'
+	tasks_config = 'config/tasks.yaml'
+
+	@before_kickoff # Optional hook to be executed before the crew starts
+	def parse_pdf(self, inputs):
+		"""
+		Logic for pdf parsing using custom pypdfform
+		"""
+		self.pdf_obj = inputs['pdf_form_schema']
+		pdf_form_schema = PdfWrapper(self.pdf_obj).schema
+		pdf_form_schema_json = json.dumps(pdf_form_schema, indent=4, sort_keys=True)
+		modified_inputs = {
+        "user_response": inputs['user_response'],
+        "pdf_form_schema": pdf_form_schema_json,
+    	}
+		return modified_inputs
+
+	@after_kickoff # Optional hook to be executed after the crew has finished
+	def fill_pdf(self, output):
+		"""
+		Logic to validate and process the output JSON
+		"""
+		try:
+			# Convert CrewOutput to JSON string
+			output_str = output.to_dict()
+			if output_str is None:
+				return None
+			filled_res = {}
+			for dic in output_str['response']:
+				if dic['answer'] != "":
+					filled_res[dic['field_key']] = dic['answer']
+			filled = PdfWrapper(self.pdf_obj).fill(
+				filled_res
+			)
+			# return filled
+			with open("/Volumes/Drive D/vizafi/python/formfiller/assets/gen_pdfs/output.pdf", "wb+") as output:
+				output.write(filled.read())
+			# Optional Save the json to a file 
+			with open("/Volumes/Drive D/vizafi/python/formfiller/assets/jsons/response.json", "w") as file:
+				json.dump(output_str, file, indent=4, sort_keys=True)
+			return 'Form filled successfully'
+		
+		except AttributeError as e:
+			print(f"Error converting CrewOutput to JSON: {e}")
+			return None
+	@agent
+	def form_filler(self) -> Agent:
+		return Agent(
+			config=self.agents_config['form_filler'],
+			# tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
+			verbose=True
+		)
+	@task
+	def form_filling_task(self) -> Task:
+		config=self.tasks_config['form_filling_task']
+		config['output_json'] = outputPydantic
+		return Task(
+			config=config,
+		)
+
+	@crew
+	def crew(self) -> Crew:
+		"""Creates the Formfiller crew"""
+		return Crew(
+			agents=self.agents, # Automatically created by the @agent decorator
+			tasks=self.tasks, # Automatically created by the @task decorator
+			process=Process.sequential,
+			verbose=False,
+			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+		)
 app = FastAPI()
+
+class UserResponseItem(BaseModel):
+    question: str
+    answer: Union[str, int, bool]
 
 class InputData(BaseModel):
     pdf_form_schema: str
-    user_response: Dict[str, Any]
+    user_response: List[UserResponseItem]
 
 @app.post("/kickoff")
 async def kickoff(input_data: InputData):
@@ -22,7 +110,3 @@ async def kickoff(input_data: InputData):
         return {"message": result}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ src/formfiller/test_server.py
@@ -0,0 +1,18 @@
+import requests
+import json
+import base64
+
+
+if __name__ == "__main__":
+    pdf_path = '/Volumes/Drive D/vizafi/python/formfiller/assets/raw_pdfs/i-90.pdf'
+    with open(pdf_path, "rb") as pdf_file:
+        pdf_binary = pdf_file.read()
+    user_response_path = '/Volumes/Drive D/vizafi/python/formfiller/assets/jsons/result.json'
+    with open(user_response_path, "r") as file:
+        user_response = json.load(file)
+    inputs = {
+        "pdf_form_schema": base64.b64encode(pdf_binary).decode('utf-8'),
+        "user_response": user_response,
+    }
+    response = requests.post('http://localhost:8000/kickoff', json=inputs)
+    print(response.json())
